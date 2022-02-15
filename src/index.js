3c35429119eb99849fc9d79e6b5899bcddd58e00
diksriva <dikshasri13@gmail.com>
2022-02-15 15:28:19 +0530

    finalbirthday

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import AppTest from './AppTest';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <AppTest /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
